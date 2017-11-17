bf367996908cb7ecc11565e50c93dba9f5a63c32
Elenazuokaite <elena.zuokaite@gmail.com>
2017-11-17 14:58:26 +0200

    Second edition

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,15 +2,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-// import { StatisticsComponent } from './statistics/statistics.component';
 import { CampaignsComponent } from './campaigns/campaigns.component';
+import { AddCampaignComponent } from './campaigns/add-campaign/add-campaign.component';
+import { StatisticsMainComponent } from './statistics-main/statistics-main.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 const APP_Routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: 'profile', component: UserProfileComponent},
-//   {path: '/statistics', component: StatisticsComponent},
   {path: 'campaigns', component: CampaignsComponent},
+  {path: 'add-campaigns', component: AddCampaignComponent},
+  {path: 'statistics-chart', component: StatisticsMainComponent},
+  {path: 'auth', component: AuthComponent},  
 ]
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_Routes);
